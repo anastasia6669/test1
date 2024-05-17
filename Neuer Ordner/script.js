45aee45b0a8aa2254331609f71562cc80ff388fd
Anastasia <anastasiashulipina@gmail.com>
2024-05-17 20:35:18 +0200

    First commit

---
+++ Neuer Ordner/script.js
@@ -0,0 +1,29 @@
+'use strict'
+let a = 5;
+console.log(a);
+let arr = ['apple','banana','grape'];
+//console.log(arr[0]);
+//alert("Hello world");
+
+//let answer = confirm("Are ou over 18?");
+//console.log(answer);
+
+//let answer = +prompt("Are you over 18?", "Da");
+//console.log(typeof(answer));
+
+
+//console.log("arr" + " - object");
+//console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(--decr);
+console.log(5%2);
+let isChecked = false,
+    isCalled = false;
+
+    console.log(isChecked || isCalled);
+
